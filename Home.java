287a2b823ec9f85d7f22a9fd8558d8e7f8415cc5
root <root@ip-172-31-45-209.ap-south-1.compute.internal>
2020-09-16 02:07:09 +0000

    Part of my new projevt

---
+++ Home.java
@@ -0,0 +1,8 @@
+class Home
+{
+	        public static void main(String []args)
+			        {
+					                System.out.println("you login name is +");
+							        }
+}
+
---
+++ Login.java
@@ -0,0 +1,7 @@
+class Login
+{
+	public static void main(String []args)
+	{
+		System.out.println("you login name is +");
+	}
+}
